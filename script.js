080aa86560facb3c14c4fe19a25b801c42e3415a
Bhumika2101 <bhumikasalunkhe283@gmail.com>
2025-02-24 03:52:26 +0530

    Updated click functionality

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
         const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
         const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
 
-        if (!audio) return; // Stop if no matching audio file
-        audio.currentTime = 0; // Rewind to start
+        if (!audio) return; 
+        audio.currentTime = 0; 
         audio.play();
         key.classList.add("playing");
     }
@@ -14,7 +14,15 @@ document.addEventListener("DOMContentLoaded", function () {
         this.classList.remove("playing");
     }
 
+    //for mouse click
     const keys = document.querySelectorAll(".key");
+    keys.forEach((key) => 
+        key.addEventListener("click", function () {
+            const keyCode = this.getAttribute("data-key");
+            playSound({ keyCode });
+        })
+    );
+    //for keyboard
     keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 
     window.addEventListener("keydown", playSound);
